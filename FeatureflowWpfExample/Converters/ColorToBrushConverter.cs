24d8e88b3c7e63b80766fd57476eacb8e51be62a
Александр Назин <a.nazin@2gis.ru>
2019-02-12 04:07:13 +0700

    [#] ColorToBrushConverter

---
+++ FeatureflowWpfExample/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FeatureflowWpfExample.Converters
+{
+    public class ColorToBrushConverter
+        : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string s)
+            {
+                try
+                {
+                    return (SolidColorBrush)(new BrushConverter().ConvertFromString(s));
+                }
+                catch (Exception) { }
+            }
+
+            return new SolidColorBrush(Colors.Black);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
